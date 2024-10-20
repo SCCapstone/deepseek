5805b598d5f62276c32a257f1ba1cc924255ffce
jakeR-wq <jacobr10183@gmail.com>
2024-10-20 01:33:18 -0400

    added home, login, and register pages to front end, #17

---
+++ frontend/src/Home.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "./App.css";
+function Home() {
+  // for navigation
+  const navigate = useNavigate();
+  return (
+    <div className="app-container">
+      <header className="app-header">
+        <h1 className="app-title">Calendar App</h1>
+        <p className="app-tagline">Look at what other people are doing!</p>
+      </header>
+      <div className="button-container">
+        <button
+          className="auth-button login-button"
+          onClick={() => navigate("/login")}
+        >
+          Login
+        </button>
+        <button
+          className="auth-button register-button"
+          onClick={() => navigate("/register")}
+        >
+          Register
+        </button>
+      </div>
+    </div>
+  );
+}
+export default Home;
