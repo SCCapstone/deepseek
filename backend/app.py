73160461da78f046363d52392cdb4be557aafa2d
jakeR-wq <jacobr10183@gmail.com>
2024-10-30 15:22:42 -0400

    refactored and commented login and backend

--- backend/app.py
+++ backend/app.py
@@ -94,15 +94,15 @@ def add_user(username: str, password: str, name: str = None, bio: str = None,
              following: list = [], auth_tokens: list = None):
     """
     inserts a user into the user collection with the following attributes
-     - "name"
-     - "username"
-     - "hashed_password"
-     - "bio"
-     - "events"
-     - "default_event_visibility"
-     - "followers"
-     - "following"
-     - "auth_tokens"
+     - name : name of the user
+     - username : username of the user
+     - hashed_password : hashed password of the user
+     - bio : bio of the user
+     - events : list of events created by the user
+     - default_event_visibility : boolean indicating whether the user's events are public by default
+     - followers : list of users who follow the user
+     - following : list of users that the user is following
+     - auth_tokens : list of authentication tokens for the user
     """
 
     user_doc = {'name' : name, 'username' : username, 'hashed_password' : password, 'bio' : bio,
@@ -113,7 +113,7 @@ def add_user(username: str, password: str, name: str = None, bio: str = None,
 def get_user(username : str, password : str = None):
     """
     returns the user with the given username and password
-    if no password given, just returns based on the username
+    if no password given, just searches and returns based on the username
     """
     if not password == None:
         return db.users.find_one({'username': username},
@@ -126,7 +126,14 @@ def add_event(user_id : uuid, title : str, description : str,
               start_time : str, end_time : str, visibility : bool = False,
               comments : list = None):
     """
-    adds an event to the user's events list
+    adds an event to the user's events list with attributes
+     - user_id : the id of the user who created the event
+     - title : the title of the event
+     - description : the description of the event
+     - start_time : the start time of the event
+     - end_time : the end time of the event
+     - visibility : whether the event is visible to others or not
+     - comments : a list of comments on the event
     """
     event_doc = {'user_id' : user_id,'title' : title, 'description' : description,
                  'start_time' : start_time, 'end_time' : end_time,
