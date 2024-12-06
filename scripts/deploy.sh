918093162ba121391c5a9346b9064fb6082e3a54
ian-turner <i.r.turner55@gmail.com>
2024-12-06 17:09:51 -0500

    fixed api url issue

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,2 +1,3 @@
 rsync -a . --exclude data --exclude 'frontend/node_modules' root@capstone.ian-turner.xyz:/app
-ssh root@capstone.ian-turner.xyz 'cd /app && docker-compose -f docker-compose.prod.yml up -d'
+ssh root@capstone.ian-turner.xyz 'cd /app && docker-compose \
+	-f docker-compose.prod.yml down && docker-compose -f docker-compose.prod.yml up -d'
