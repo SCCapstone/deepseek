24c4f73e0d382066be839d3ccdf06bb3f4f80720
ian-turner <i.r.turner55@gmail.com>
2024-12-06 14:16:12 -0500

    updated deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,7 @@
-cd frontend && npm i && npm run build && cd ..
+cd frontend
+echo 'REACT_APP_API_URL=http://api.capstone.ian-turner.xyz' > .env
+npm i
+npm run build
+cd ..
 rsync -a . --exclude 'frontend/node_modules' root@capstone.ian-turner.xyz:/app
 ssh root@capstone.ian-turner.xyz 'cd /app && docker-compose -f docker-compose.prod.yml up -d'
