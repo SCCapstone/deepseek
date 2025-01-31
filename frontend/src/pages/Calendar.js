a541aadda45f3bf6a5bfe682f4dd2e3c7d0d4496
ian-turner <i.r.turner55@gmail.com>
2025-01-31 18:02:19 -0500

    restyled create event page

--- frontend/src/pages/Calendar.js
+++ frontend/src/pages/Calendar.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect} from "react";
 import { useNavigate } from 'react-router-dom';
 import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+
 import NavBar from '../components/NavBar.js';
 import EventList from '../components/EventList.js';
-
 import api from '../lib/api.js';
 
 
@@ -30,22 +31,23 @@ export default function CalendarPage() {
   
     const handleDateChange = (date) => {
         setSelectedDate(date);
-        console.log(date)
     }
   
-    if (loading) return <div>Loading...</div>;
     if (error) return <div>Error: {error}</div>;
   
     return (
         <div style={{height: '100vh'}}>
             <NavBar/>
             {loading ? <div>Loading...</div> :
-                <div className='h-100 d-flex flex-row'>
+                <div style={{height: '100%'}} className='d-flex flex-row'>
+                    <div className='w-100 h-100 d-flex flex-column'>
                     <Calendar
+                        className='w-100 h-100'
                         onChange={handleDateChange}
                         value={selectedDate}
-                        view="month"
+                        view='month'
                     />
+                    </div>
                     <EventList events={events.filter(event => {
                         const eventDate = new Date(event.start_time).toDateString();
                         return eventDate === selectedDate.toDateString();
--- frontend/src/pages/CreateEvent.js
+++ frontend/src/pages/CreateEvent.js
@@ -1,159 +1,161 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import NavBar from '../components/NavBar.js';
+import api from '../lib/api.js';
 
-const CreateEvent = () => {
-  const [eventDetails, setEventDetails] = useState({
-    title: '',
-    description: '',
-    day: '',
-    location: '',
-    startTime: '',
-    endTime: '',
-    reminder: false,
-  });
 
-  const handleChange = (e) => {
-    const { name, value, type, checked } = e.target;
-    setEventDetails({
-      ...eventDetails,
-      [name]: type === 'checkbox' ? checked : value,
-    });
-  };
-
-  const handleSubmit = async (e) => {
-    const start = new Date(eventDetails.day + ' ' + eventDetails.startTime);
-    const end = new Date(eventDetails.day + ' ' + eventDetails.endTime);
-    console.log(start);
-    e.preventDefault();
-    const res = await fetch(process.env.REACT_APP_API_URL + '/addevent', {
-      method: 'post',
-      mdoe: 'cors',
-      credentials: 'include',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        title: eventDetails.title,
-        description: eventDetails.description,
-        start_time: start.toISOString(),
-        end_time: end.toISOString(),
-        reminder: eventDetails.reminder,
-      })
+export default function CreateEvent() {
+    const navigate = useNavigate();
+    const [eventDetails, setEventDetails] = useState({
+        title: '',
+        description: '',
+        day: '',
+        location: '',
+        startTime: '',
+        endTime: '',
+        reminder: false,
     });
 
-    if (!res.ok) {
-      // do something with error
-      alert('Could not create event');
-    }
-    else {
-      alert('Event created!');
-    }
-  };
+    const handleChange = (e) => {
+        const { name, value, type, checked } = e.target;
+        setEventDetails({
+            ...eventDetails,
+            [name]: type === 'checkbox' ? checked : value,
+        });
+    };
 
-  const handleCancel = () => {
-    setEventDetails({
-      title: '',
-      description: '',
-      day: '',
-      location: '',
-      startTime: '',
-      endTime: '',
-      reminder: false,
-    });
-  };
+    async function handleSubmit(event) {
+        event.preventDefault();
+        const start = new Date(eventDetails.day + ' ' + eventDetails.startTime);
+        const end = new Date(eventDetails.day + ' ' + eventDetails.endTime);
+        const newEvent = {
+            title: eventDetails.title,
+            description: eventDetails.description,
+            start_time: start.toISOString(),
+            end_time: end.toISOString(),
+            reminder: eventDetails.reminder,
+        }
+        const { data, error } = await api.post('/addevent', newEvent);
+        if (error) {
+            // handle error here
+            alert('Could not create event');
+        }
+        else {
+            alert('Event created');
+            navigate('/calendar');
+        }
+    };
 
-  return (
-    <div className="create-event-container">
-      <NavBar/>
-      <h1>Create an Event</h1>
-      <form onSubmit={handleSubmit} className="event-form">
-        <div className="form-group">
-          <label htmlFor="title">Title:</label>
-          <input
-            type="text"
-            id="title"
-            name="title"
-            value={eventDetails.title}
-            onChange={handleChange}
-            required
-          />
+    const handleCancel = () => {
+        setEventDetails({
+            title: '',
+            description: '',
+            day: '',
+            location: '',
+            startTime: '',
+            endTime: '',
+            reminder: false,
+        });
+    };
+
+    return (
+        <div>
+            <NavBar/>
+            <form onSubmit={handleSubmit} className='container shadow rounded-lg mt-5 p-3'>
+                <h1>Create an Event</h1>
+                <div className='mb-3'>
+                    <label htmlFor="title">Title</label>
+                    <input
+                        className='form-control'
+                        type="text"
+                        id="title"
+                        name="title"
+                        value={eventDetails.title}
+                        onChange={handleChange}
+                        required
+                    />
+                </div>
+                <div className='mb-3'>
+                    <label htmlFor="description">Description:</label>
+                    <textarea
+                        className='form-control'
+                        id="description"
+                        name="description"
+                        value={eventDetails.description}
+                        onChange={handleChange}
+                        required
+                    ></textarea>
+                </div>
+                <div className='mb-3'>
+                    <label htmlFor="location">Location:</label>
+                    <input
+                        className='form-control'
+                        type="text"
+                        id="location"
+                        name="location"
+                        value={eventDetails.location}
+                        onChange={handleChange}
+                        required
+                    />
+                </div>
+                <div className='d-flex flex-row justify-content-start mb-3'>
+                    <div className='mr-3'>
+                        <label htmlFor="day">Day:</label>
+                        <input
+                            className='form-control'
+                            type="date"
+                            id="day"
+                            name="day"
+                            value={eventDetails.day}
+                            onChange={handleChange}
+                            required
+                        />
+                    </div>
+                    <div className='mr-3'>
+                        <label htmlFor="startTime">Start Time:</label>
+                        <input
+                            className='form-control'
+                            type="time"
+                            id="startTime"
+                            name="startTime"
+                            value={eventDetails.startTime}
+                            onChange={handleChange}
+                            required
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="endTime">End Time:</label>
+                        <input
+                            className='form-control'
+                            type="time"
+                            id="endTime"
+                            name="endTime"
+                            value={eventDetails.endTime}
+                            onChange={handleChange}
+                            required
+                        />
+                    </div>
+                </div>
+                <div className='mb-3'>
+                    <label className='form-control'>
+                        <input
+                            type="checkbox"
+                            name="reminder"
+                            checked={eventDetails.reminder}
+                            onChange={handleChange}
+                        />
+                        Set Reminder
+                    </label>
+                </div>
+                <div>
+                    <button type="submit" className="btn btn-primary mr-3">
+                        Save Event
+                    </button>
+                    <button type="button" onClick={handleCancel} className="btn btn-secondary">
+                        Cancel
+                    </button>
+                </div>
+            </form>
         </div>
-        <div className="form-group">
-          <label htmlFor="description">Description:</label>
-          <textarea
-            id="description"
-            name="description"
-            value={eventDetails.description}
-            onChange={handleChange}
-            required
-          ></textarea>
-        </div>
-        <div className="form-group">
-          <label htmlFor="day">Day:</label>
-          <input
-            type="date"
-            id="day"
-            name="day"
-            value={eventDetails.day}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="location">Location:</label>
-          <input
-            type="text"
-            id="location"
-            name="location"
-            value={eventDetails.location}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="startTime">Start Time:</label>
-          <input
-            type="time"
-            id="startTime"
-            name="startTime"
-            value={eventDetails.startTime}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="endTime">End Time:</label>
-          <input
-            type="time"
-            id="endTime"
-            name="endTime"
-            value={eventDetails.endTime}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label>
-            <input
-              type="checkbox"
-              name="reminder"
-              checked={eventDetails.reminder}
-              onChange={handleChange}
-            />
-            Set Reminder
-          </label>
-        </div>
-        <div className="form-actions">
-          <button type="submit" className="btn btn-primary">
-            Save Event
-          </button>
-          <button type="button" onClick={handleCancel} className="btn btn-secondary">
-            Cancel
-          </button>
-        </div>
-      </form>
-    </div>
-  );
+    );
 };
-
-export default CreateEvent;
