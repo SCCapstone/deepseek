5c7fdd16fa150957224336dad030f5f2550739de
ian-turner <i.r.turner55@gmail.com>
2025-03-02 17:03:10 -0500

    added event comments api routes to backend

--- backend/db/__init__.py
+++ backend/db/__init__.py
@@ -3,3 +3,4 @@ from .user import User
 from .event import Event
 from .friend_relation import FriendRelation
 from .auth_token import AuthToken
+from .event_comment import EventComment
--- backend/routers/event.py
+++ backend/routers/event.py
@@ -4,7 +4,7 @@ Flask routes for event-related API endpoints
 import logging
 from flask import Blueprint, request, make_response
 
-from db import Event
+from db import Event, User
 from utils.auth_utils import *
 from utils.data_utils import *
 from utils.error_utils import *
@@ -25,7 +25,7 @@ logger = logging.getLogger(__name__)
     'reminder': {'type': bool},
     'public': {'type': bool},
 })
-def add_event(current_user):
+def add_event(current_user: User):
     event_data = request.json
     event_data['user_id'] = current_user._id
     Event.create(**event_data)
@@ -42,24 +42,23 @@ def get_events(current_user):
 
 @event_router.route('/get-event/<event_id>', methods=['GET'])
 @login_required
-def get_event(current_user, event_id):
-    # finding event in user's own events
-    user_event = Event.find_one(_id=ObjectId(event_id), user_id=current_user._id)
-    if user_event:
-        return make_response({'data': user_event.to_dict()})
-
-    # getting ids of all user's friends    
-    user_friends = current_user.friends
-    friend_ids = [x._id for x in user_friends]
-
-    # finding event within friend events
-    friend_event = Event.find_one(_id=ObjectId(event_id), user_id=friend_ids)
-    if friend_event:
-        return make_response({'data': friend_event.to_dict()})
+def get_event(current_user: User, event_id: str):
+    # finding event in database
+    event = Event.find_one(_id=ObjectId(event_id))
+    if not event:
+        raise NotFoundError('Invalid event id `%s`' % event_id)
     
-    # event not found
-    raise NotFoundError('Invalid event id `%s`' % event_id)
+    # making sure user has read access
+    event_user = User.find_one(_id=event.user_id)
+    if event_user != current_user:
+        # if the event is not owned by the user, then the user can only
+        # view if it is owned by a friend
+        friend_status = current_user.get_friend_status(event_user)
+        if friend_status != 'friend':
+            raise ForbiddenError('You do not have access to this event')
     
+    return make_response({'data': event.to_dict()})
+
 
 @event_router.route('/update-event/<event_id>', methods=['POST'])
 @login_required
@@ -72,18 +71,59 @@ def get_event(current_user, event_id):
     'reminder': {'type': bool},
     'public': {'type': bool},
 })
-def update_event(current_user, event_id):
+def update_event(current_user: User, event_id: str):
     data = request.json
     event = Event.find_one(_id=ObjectId(event_id), user_id=current_user._id)
     if not event:
         raise NotFoundError('Invalid event id `%s`' % event_id)
-    event.update(data)
+    event.update(**data)
     return make_response({'message': 'Event updated'})
+
+
+@event_router.route('/get-event-comments/<event_id>')
+@login_required
+def get_event_comments(current_user: User, event_id: str):
+    event = Event.find_one(_id=ObjectId(event_id))
+    if not event:
+        raise NotFoundError('Invalid event id `%s`' % event_id)
+    
+    event_user = User.find_one(_id=event_id)
+    if current_user != event_user:
+        friend_status = current_user.get_friend_status(event_user)
+        if friend_status != 'friend':
+            raise ForbiddenError('You do not have access to this event')
+    
+    comments = event.comments
+    comment_data = [x.to_dict() for x in comments]
+    for x in comment_data:
+        comment_user = User.find_one(_id=x['user_id'])
+        x['user'] = comment_user.profile
+        del x['user_id']
+        
+    return make_response({'data': comment_data})
+
+
+@event_router.route('/add-event-comment/<event_id>', methods=['POST'])
+@login_required
+@data_filter({'body': {'type': str}})
+def add_event_comment(current_user: User, event_id: str):
+    event = Event.find_one(_id=ObjectId(event_id))
+    if not event:
+        raise NotFoundError('Invalid event id `%s`' % event_id)
+    
+    event_user = User.find_one(_id=event.user_id)
+    if current_user != event_user:
+        friend_status = current_user.get_friend_status(event_user)
+        if friend_status != 'friend':
+            raise ForbiddenError('You do not have access to this event')
+    
+    event.add_comment(current_user._id, body)
+    return make_response({'message': 'Successfully commented on event'}, 201)
 
 
 @event_router.route('/events/notifications')
 @login_required
-def get_notifications(current_user):
+def get_notifications(current_user: User):
     notifications = current_user.notifications
     notification_data = [x.to_dict() for x in notifications]
     return make_response({'message': 'Notifications retrieved', 'data': notification_data})
@@ -91,7 +131,7 @@ def get_notifications(current_user):
 
 @event_router.route('/events/notifications/clear', methods=['POST'])
 @login_required
-def clear_notifications(current_user):
+def clear_notifications(current_user: User):
     notifications = current_user.notifications
     if len(notifications) == 0:
         return make_response({'message': 'No notifications to clear'})
@@ -102,7 +142,7 @@ def clear_notifications(current_user):
 
 @event_router.route('/get-friends-events', methods=['GET'])
 @login_required
-def get_friends_events(current_user):
+def get_friends_events(current_user: User):
     friends = current_user.friends
     friend_events = []
     for friend in friends:
