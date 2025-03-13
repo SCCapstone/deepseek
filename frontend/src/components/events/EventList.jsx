038d3985fd85f6928c6afd0ce9f23f37921ec434
ian-turner <i.r.turner55@gmail.com>
2025-03-13 14:33:36 -0400

    fixed date display in calendar sidebar

--- frontend/src/components/events/EventList.jsx
+++ frontend/src/components/events/EventList.jsx
@@ -6,6 +6,12 @@ function Event({ event }) {
     const navigate = useNavigate();
 
     const handleClick = () => navigate('/events/' + event.id);
+    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const dateParts = event.date.split('-');
+    const year = parseInt(dateParts[0]);
+    const month = parseInt(dateParts[1]) - 1;
+    const day = parseInt(dateParts[2]);
+    const eventDate = new Date(year, month, day);
 
     return (
         <div
@@ -20,7 +26,7 @@ function Event({ event }) {
                         <p className='m-0 text-muted'>@{event.user.username}</p>
                     : null}
                 </div>
-                <p className='text-muted m-0'>{(new Date(event.date)).toDateString()}</p>
+                <p className='text-muted m-0'>{eventDate.toDateString()}</p>
                 <p className='text-muted m-0'>
                     {event.start_time && event.start_time.length > 0 ? (
                         event.start_time + '-' + event.end_time
@@ -33,9 +39,35 @@ function Event({ event }) {
 
 
 export default function EventList({ events }) {
+    const sortedEvents = events.sort((a, b) => {
+        const dateA = new Date(a.date);
+        const dateB = new Date(b.date);
+        if (a.date === b.date) {
+            const hasStartTimeA = a.start_time && a.start_time.length > 0;
+            const hasStartTimeB = b.start_time && b.start_time.length > 0;
+
+            if (!hasStartTimeA)
+                return -1;
+
+            if (!hasStartTimeB)
+                return 1;
+
+            const hoursA = parseInt(a.start_time.split(':')[0]);
+            const minsA = parseInt(a.start_time.split(':')[1]);
+            const hoursB = parseInt(b.start_time.split(':')[0]);
+            const minsB = parseInt(b.start_time.split(':')[1]);
+
+            dateA.setHours(hoursA);
+            dateA.setMinutes(minsA);
+            dateB.setHours(hoursB);
+            dateB.setMinutes(minsB);
+        }
+        return dateA - dateB;
+    });
+
     return (
         <div className='p-2 w-100 d-flex flex-column'>
-            {events.map((event, i) => <Event key={i} event={event}/>)}
+            {sortedEvents.map((event, i) => <Event key={i} event={event}/>)}
         </div>
     );
 }
