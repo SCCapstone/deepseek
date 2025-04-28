a9a83cde6792dfd9732d33a0ebaff85b9eae8acc
ian-turner <i.r.turner55@gmail.com>
2025-04-27 22:48:18 -0400

    login and register tests

--- backend/tests/test_app.py
+++ backend/tests/test_app.py
@@ -1,17 +1,56 @@
 import os
 import sys
+import json
+import random
+import string
+import pytest
 
 # adding the parent directory to sys.path
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-import pytest
 from app import app
+
+
+def generate_random_string(length):
+    """
+    Helper function to generate random usernames/passwords
+    """
+    characters = string.ascii_letters + string.digits
+    random_string = ''.join(random.choice(characters) for _ in range(length))
+    return random_string
+
+
+# generating a random string to use for login details
+rand_str = generate_random_string(10)
 
 
 @pytest.fixture
 def client():
     """Set up a test client"""
     print('Setting up client')
-    with app.test_client():
+    with app.test_client() as client:
         yield client
     print('Tearing down client')
+
+
+def test_register(client):
+    """Testing the user register route"""
+    data = {
+        'email': '%s@%s.com' % (rand_str, rand_str),
+        'username': rand_str,
+        'password': rand_str,
+    }
+    response = client.post('/register', data=json.dumps(data), \
+                           headers={'Content-Type': 'application/json'})
+    assert response.status_code == 201
+
+
+def test_login(client):
+    """Testing the user login route"""
+    data = {
+        'username': rand_str,
+        'password': rand_str,
+    }
+    response = client.post('/login', data=json.dumps(data), \
+                           headers={'Content-Type': 'application/json'})
+    assert response.status_code == 200
