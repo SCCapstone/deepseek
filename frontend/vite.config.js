ae5ad99ca10db27fcacb3f7edddbe9d421af390e
Tristan Shillingford <trshillingford@gmail.com>
2025-04-27 22:50:54 -0400

    Unit tests for SettingsWindow

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,6 +6,13 @@ export default defineConfig({
     plugins: [
         react(),
     ],
+    test: {
+        globals: true, // This enables global `describe`, `it`, `expect` functions
+        environment: 'jsdom', // Use jsdom for DOM testing
+        coverage: {
+          provider: 'c8', // Optional: code coverage support
+        },
+    },
     server: {
         host: true,
         port: 4000,
